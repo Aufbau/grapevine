1917322c0866c16ecc9b1623c78a207991965dee
nishnat.rishi <nishnat.rishi@gmail.com>
2021-06-11 12:26:45 +0530

    Added react-router-dom (boilerplate).

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,30 @@ import React, { useEffect, useState } from "react"
 import networkService from "./services/networkService"
 
 const App = () => {
-  const [posts, setPosts] = useState([])
+  const[displayPosts, setDisplayPosts] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
+      const fetchedUsers = await networkService.get("/users")
       const fetchedPosts = await networkService.get("/posts")
-      setPosts(fetchedPosts)
+
+      const data = []
+      fetchedPosts.map(post => {
+        data.push({
+          ...post,
+          user: fetchedUsers.find(user => user.id === post.userId)
+        })
+      })
+      setDisplayPosts(data)
     }
     fetchData()
   }, [])
-
+  
   return (
     <div>
-      {posts.map(post =>
-        <p key={post.title}>
-          {post.title}
+      {displayPosts.map(post =>
+        <p key={post.id}>
+          {post.title} - <em>{post.user.name}</em>
         </p>
       )}
     </div>
