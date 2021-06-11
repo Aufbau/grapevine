bbe08943905adcffc196b2e8f66569e0e7ef3a80
nishnat.rishi <nishnat.rishi@gmail.com>
2021-06-11 09:36:29 +0530

    Added more boilerplate.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(<div>Hello, world!</div>, 
-  document.getElementById('root')
-);
+ReactDOM.render(
+  <div>Hello, world!</div>, 
+  document.getElementById("root")
+)
